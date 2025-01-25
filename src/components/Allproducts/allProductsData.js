e3c0c289ceaeafacbfb2fbb3367669724d697b4e
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-25 19:14:41 +0500

    Update allProductsData.js

--- src/components/Allproducts/allProductsData.js
+++ src/components/Allproducts/allProductsData.js
@@ -3,7 +3,7 @@ const allProductsData = {
     {
       id: 1,
       discount: 50,
-      img: "/assets/flash-deals/",
+      img: "/assets/flash-deals/flash-1.png",
       name: "Jaket",
       price: 100,
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit, neque vitae egestas porta, eros neque vestibulum purus, at pretium nulla tellus sed mauris. Aliquam nec lacus non metus bibendum faucibus. Etiam vulputate leo nec ante consectetur, sed vulputate dui vestibulum. Quisque porta augue sapien, et lobortis odio molestie mattis. Aliquam erat volutpat. Morbi semper lacus et augue aliquam pretium. Sed interdum risus eu sem gravida malesuada. Suspendisse efficitur elementum ex, sed varius tortor auctor eu. Ut interdum posuere lacus, sed tristique erat blandit et. Nulla ac odio ullamcorper, vestibulum urna sed, tristique turpis. Pellentesque maximus porttitor molestie. Nunc consequat.",
